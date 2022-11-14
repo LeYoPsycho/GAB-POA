cc5ea3d4819fd1f3e964a4ee7d897302f67c871f
Gabbi2009 <Gabbi2009@LAPTOP-066LHEIS>
2022-11-14 10:57:14 +0100

    Projektdateien hinzufügen.

---
+++ frmMain.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace GAB_POA
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblHeadline = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.gbMainInfo = new System.Windows.Forms.GroupBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.gbMainInfo.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblHeadline
+            // 
+            this.lblHeadline.AutoSize = true;
+            this.lblHeadline.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblHeadline.Location = new System.Drawing.Point(208, 9);
+            this.lblHeadline.Name = "lblHeadline";
+            this.lblHeadline.Size = new System.Drawing.Size(500, 81);
+            this.lblHeadline.TabIndex = 0;
+            this.lblHeadline.Text = "Potenzialanalyse";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 221);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(109, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Wunschbereich";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(52, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Schule";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Aktuelles Datum";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Datum der POA";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Geburtsdatum";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nachname";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vorname";
+            // 
+            // gbMainInfo
+            // 
+            this.gbMainInfo.Controls.Add(this.textBox7);
+            this.gbMainInfo.Controls.Add(this.textBox6);
+            this.gbMainInfo.Controls.Add(this.textBox5);
+            this.gbMainInfo.Controls.Add(this.textBox4);
+            this.gbMainInfo.Controls.Add(this.textBox3);
+            this.gbMainInfo.Controls.Add(this.textBox2);
+            this.gbMainInfo.Controls.Add(this.textBox1);
+            this.gbMainInfo.Controls.Add(this.label7);
+            this.gbMainInfo.Controls.Add(this.label6);
+            this.gbMainInfo.Controls.Add(this.label1);
+            this.gbMainInfo.Controls.Add(this.label5);
+            this.gbMainInfo.Controls.Add(this.label2);
+            this.gbMainInfo.Controls.Add(this.label4);
+            this.gbMainInfo.Controls.Add(this.label3);
+            this.gbMainInfo.Location = new System.Drawing.Point(12, 93);
+            this.gbMainInfo.Name = "gbMainInfo";
+            this.gbMainInfo.Size = new System.Drawing.Size(370, 279);
+            this.gbMainInfo.TabIndex = 2;
+            this.gbMainInfo.TabStop = false;
+            this.gbMainInfo.Text = "Wichtige Informationen";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(164, 218);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(181, 27);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(164, 185);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(181, 27);
+            this.textBox6.TabIndex = 12;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(164, 152);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(181, 27);
+            this.textBox5.TabIndex = 11;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(164, 119);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(181, 27);
+            this.textBox4.TabIndex = 10;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(164, 86);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(181, 27);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(164, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(181, 27);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(164, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PlaceholderText = "Max Mustermensch";
+            this.textBox1.Size = new System.Drawing.Size(181, 27);
+            this.textBox1.TabIndex = 7;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(939, 644);
+            this.Controls.Add(this.gbMainInfo);
+            this.Controls.Add(this.lblHeadline);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmMain";
+            this.Text = "Potenzialanalyse";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.gbMainInfo.ResumeLayout(false);
+            this.gbMainInfo.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblHeadline;
+        private Label label7;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private GroupBox gbMainInfo;
+        private TextBox textBox7;
+        private TextBox textBox6;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,15 @@
+namespace GAB_POA
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
